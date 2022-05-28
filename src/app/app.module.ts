61850ef66938f6baa523fc62c398ca9382a1a1ad
Malwina Leśniewska <malwina.lesniewska@gmail.com>
2022-05-28 11:53:46 +0200

    added bootstrap; added project structure and base mixins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleSectionComponent } from './pages/title-section/title-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleSectionComponent
   ],
   imports: [
     BrowserModule
